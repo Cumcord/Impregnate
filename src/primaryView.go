e4854bdc07d4d529ee4a508cbadc028854906c21
Alyxia Sother <lexisoth2005@gmail.com>
2021-09-27 12:41:59 +0200

    [primaryView] Add basic installation UI

--- src/primaryView.go
+++ src/primaryView.go
@@ -8,12 +8,46 @@ import (
 )
 
 func (app *UpApplication) ShowPrimaryView() {
-	slots := []framework.FlexboxSlot{}
-
-	slots = append(slots, framework.FlexboxSlot{
-		Element: framework.NewUILabelPtr(integration.NewTextTypeChunk("Welcome to the Cumcord installer!", design.GlobalFont), 0xFFFFFFFF, 0, frenyard.Alignment2i{}),
-		Grow:    1,
-	})
+	slots := []framework.FlexboxSlot{
+		{
+			Grow: 1,
+		},
+		{
+			Element: framework.NewUILabelPtr(integration.NewTextTypeChunk("Welcome to the Cumcord installer!", design.GlobalFont), 0xFFFFFFFF, 0, frenyard.Alignment2i{}),
+		},
+		{
+			Basis:  frenyard.Scale(design.DesignScale, 32),
+			Shrink: 1,
+		},
+		{
+			Element: framework.NewUIFlexboxContainerPtr(framework.FlexboxContainer{
+				DirVertical: false,
+				Slots: []framework.FlexboxSlot{
+					{
+						Grow: 1,
+					},
+					{
+						Element: design.ButtonAction(design.ThemeOkActionButton, "Install", func() {}),
+						Shrink:  1,
+					},
+					{
+						Basis:  frenyard.Scale(design.DesignScale, 32),
+						Shrink: 1,
+					},
+					{
+						Element: design.ButtonAction(design.ThemeRemoveActionButton, "Uninstall", func() {}),
+						Shrink:  1,
+					},
+					{
+						Grow: 1,
+					},
+				},
+			}),
+		},
+		{
+			Grow: 1,
+		},
+	}
 
 	app.Teleport(design.LayoutDocument(design.Header{
 		Title:       "Impregnate",
